88f21fbcf2c4ae8acd06588998e111a77bf27c08
YeonJu Hong <hyj9785@likelion.org>
2018-07-17 09:45:19 +0000

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: "213", email: "1@1.com", password: "121212", encrypted_password: "121212")
-Post.create(title: "안녕하세요", content: "반갑습니다",user_id: 1)
+(1..3).each do |i|
+    User.create(username: i , email:"#{i}@#{i}.com" , password: "121212", password_confirmation: "121212")
+    (1..3).each do |j|
+        Post.create(title: "#{i}가 #{j}번째 글을 작성했습니다." , content: "#{i}가 쓴 #{j}번째 글입니다.", user_id: i)
+    end
+end
